50d76d955945ddb0834567e099b2da8088133ff8
smallfang <smallfang@rotriw.com>
2024-08-27 10:44:10 +0800

    init README

---
+++ README.md
@@ -0,0 +1,1 @@
+### BGMDL - Core
--- libs/download_link/src/lib.rs
+++ libs/download_link/src/lib.rs
@@ -1,5 +1,3 @@
-use std::thread;
-
 #[derive(Debug, Clone)]
 pub struct DownloadData {
     pub name: String,
@@ -8,7 +6,6 @@ pub struct DownloadData {
     pub speed: i64,
     pub eta: i64,
 }
-
 
 pub trait DownloadTools {
     // This will run in the open cli.
@@ -19,35 +16,3 @@ pub trait DownloadTools {
     fn login(&mut self, username: &str, password: &str, link: &str) -> Result<(), Box<dyn std::error::Error>>;
     fn progress_update_run(&mut self, callback: Box<dyn FnMut(DownloadData) -> () + Send>) -> ();
 }
-
-// pub struct TestTools {
-
-// }
-
-// pub struct TestManager {
-// }
-
-// impl DownloadManager for TestManager {
-//     fn start(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-//         Ok(())
-//     }
-
-//     fn progress_update_run(&mut self, mut callback: Box<dyn FnMut(DownloadData) -> () + Send>) -> () {
-//         let mut data = DownloadData {
-//             status: "Downloading".to_string(),
-//             progress: 0.0,
-//             speed: 0.0,
-//             eta: 0.0,
-//         };
-//         thread::spawn(move || {
-//             for i in 0..100 {
-//                 data.progress = i as f32;
-//                 data.speed = i as f32;
-//                 data.eta = i as f32;
-//                 callback(data.clone());
-//                 thread::sleep(std::time::Duration::from_secs(1));
-//             }
-//         });
-        
-//     }
-// }
